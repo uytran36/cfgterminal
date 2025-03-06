62ed7a2701af8334d3df3b7466cfde939bfce668
Trần Khánh Gia Uy <52996109+uytran36@users.noreply.github.com>
2025-03-07 03:11:38 +0700

    chore: update fzf preview file

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,14 @@ Import-Module -Name Terminal-Icons
 # PSFzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+# PSFzf config preview
+$env:FZF_DEFAULT_OPTS="--prompt '⯈ ' --height 95% --layout=reverse --border 
+--color=dark
+--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
+--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
+$env:FD_OPTIONS="--hidden --follow"
+$env:FZF_DEFAULT_COMMAND="fd --type f --type l {0}" -f $env:FD_OPTION
+$env:FZF_CTRL_T_OPTS='--preview "bat --style=numbers,changes,header --color=always --theme=Dracula --line-range :500 {}  || cat {}"'
 
 
 # PSReadLine
